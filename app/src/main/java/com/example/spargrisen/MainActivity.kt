f35a15611e2eea05a41eb224e5d75979fc7c801a
jonatangroncode <jonatan.gron.code@hotmail.com>
2022-10-18 17:13:13 +0200

    Lite ändringar, i layouts, manual input layout skapad, string array i values mapp till spinner i manual input layouten.

--- app/src/main/java/com/example/spargrisen/MainActivity.kt
+++ app/src/main/java/com/example/spargrisen/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.spargrisen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.core.content.ContentProviderCompat.requireContext
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
