1a3065d5cb986c6b26c98d3e82886aa59fc308ec
jonatangroncode <114836936+jonatangroncode@users.noreply.github.com>
2022-10-27 10:11:41 +0200

    Menybar och navigering mellan fragment

--- app/src/main/java/com/example/spargrisen/MainActivity.kt
+++ app/src/main/java/com/example/spargrisen/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.spargrisen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.spargrisen.databinding.ActivityGraphAndTableBinding
 import com.example.spargrisen.fragments.GraphFragment
 import com.example.spargrisen.fragments.HomeFragment
 import com.example.spargrisen.fragments.SettingsFragment
-import com.example.spargrisen.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
-import com.jjoe64.graphview.GraphView
-import kotlinx.android.synthetic.main.activity_graph_and_table.*
-import kotlinx.android.synthetic.main.activity_graph_and_table.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,33 +14,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val viewbinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_graph_and_table)
+        val viewbinding= ActivityGraphAndTableBinding.inflate(layoutInflater)
 
-//        setContentView(viewbinding.root)
-//        replaceFragment(GraphFragment())
+        setContentView(viewbinding.root)
+        replaceFragment(GraphFragment())
+        viewbinding.bottomNavigation.setOnItemSelectedListener {
 
+            when(it.itemId){
+                R.id.ic_graph ->replaceFragment(GraphFragment())
+                R.id.ic_home ->replaceFragment(HomeFragment())
+                R.id.ic_settings ->replaceFragment(SettingsFragment())
 
-//        when(it.itemId){
-//                R.id.ic_baseline_auto_graph_24 ->replaceFragment(GraphFragment())
-//                R.id.ic_baseline_home_24 ->replaceFragment(HomeFragment())
-//                R.id.ic_baseline_settings_24 ->replaceFragment(SettingsFragment())
-//
-//            }
-//            true
-//        }
-//
-//    }
-//
-//
-//    private fun replaceFragment(fragment: Fragment){
-//        if (fragment != null){
-//            val transaction = supportFragmentManager.beginTransaction()
-//            transaction.replace(R.id.fragment_container, fragment)
-//            transaction.commit()
-//        }
-//
+            }
+            true
+        }
+
     }
 
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
+
+    }
 }
 
--- app/src/main/java/com/example/spargrisen/fragments/GraphFragment.kt
+++ app/src/main/java/com/example/spargrisen/fragments/GraphFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [GraphFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GraphFragment : Fragment(R.layout.fragment_graph) {
+class GraphFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,12 +31,11 @@ class GraphFragment : Fragment(R.layout.fragment_graph) {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_graph, container, false)
-
     }
 
     companion object {
