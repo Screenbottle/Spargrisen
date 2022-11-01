71d93d8b557e8b1bc35763c27a991a8219ab0493
noelpettersson <noel.pettersson@iths.se>
2022-11-01 22:31:18 +0100

    Read from database added
    Not fully working

--- app/src/main/java/com/example/spargrisen/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/spargrisen/fragments/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.example.spargrisen.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.spargrisen.DatabaseController
 import com.example.spargrisen.R
 
 // TODO: Rename parameter arguments, choose names that match
