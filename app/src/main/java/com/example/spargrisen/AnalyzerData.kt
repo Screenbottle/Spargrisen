e94d4d2139aac28d421101bf823c47db81a8703a
Arvid <arvid.rydh@iths.se>
2022-11-03 13:48:24 +0100

    regex work started

--- app/src/main/java/com/example/spargrisen/AnalyzerData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.spargrisen
-
-import android.graphics.RectF
-
-data class AnalyzerData (
-    val onTextFound: (String) -> Unit,
-    val rectF: RectF
-
-    )
--- app/src/main/java/com/example/spargrisen/ScannedText.kt
+++
@@ -1,6 +0,0 @@
-package com.example.spargrisen
-
-
-object ScannedText {
-    var scannedText: String = ""
-}
