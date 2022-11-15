8fe95e0d70127152976d79429d99091acdb954b9
noelpettersson <noel.pettersson@iths.se>
2022-11-15 13:56:31 +0100

    Crash on manual input fixed

--- app/src/main/java/com/example/spargrisen/fragments/GraphFragment.kt
+++ app/src/main/java/com/example/spargrisen/fragments/GraphFragment.kt
@@ -29,16 +29,15 @@ class GraphFragment : Fragment() {
     var dbc = DatabaseController()
 
     lateinit var periodGraph: GraphView
-    lateinit var yearGraph: AAChartView
     lateinit var pieGraph: AAChartView
     lateinit var backButton: ImageButton
     lateinit var nextButton: ImageButton
     lateinit var dateText: TextView
+    lateinit var yearGraph: AAChartView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -49,11 +48,11 @@ class GraphFragment : Fragment() {
         var view = inflater.inflate(R.layout.fragment_graph, container, false)
 
         periodGraph = view.findViewById(R.id.period_graph)
-        yearGraph = view.findViewById(R.id.year_graph)
         pieGraph = view.findViewById(R.id.categorygraph)
         backButton = view.findViewById(R.id.backDate)
         nextButton = view.findViewById(R.id.nextDate)
         dateText = view.findViewById(R.id.dateText)
+        yearGraph = view.findViewById(R.id.year_graph)
 
 
         auth = Firebase.auth
@@ -118,54 +117,60 @@ class GraphFragment : Fragment() {
     }
 
     fun graphYear() {
+        val yearGraph = view?.findViewById<AAChartView>(R.id.year_graph)
         var janCost: Long = 0; var febCost: Long = 0; var marCost: Long = 0; var aprCost: Long = 0; var mayCost: Long = 0; var junCost: Long = 0; var julCost: Long = 0; var augCost: Long = 0; var sepCost: Long = 0; var octCost: Long = 0; var novCost: Long = 0; var decCost: Long = 0
 
-        for (i in dbc.purchasesList.indices) {
-            if (dbc.getYear(dbc.purchasesList[i].purchaseDateString) == dbc.getCurrentYear()) {
-                when (getMonth(dbc.purchasesList[i].purchaseDateString)) {
-                    1 -> janCost += dbc.purchasesList[i].purchaseCost
-                    2 -> febCost += dbc.purchasesList[i].purchaseCost
-                    3 -> marCost += dbc.purchasesList[i].purchaseCost
-                    4 -> aprCost += dbc.purchasesList[i].purchaseCost
-                    5 -> mayCost += dbc.purchasesList[i].purchaseCost
-                    6 -> junCost += dbc.purchasesList[i].purchaseCost
-                    7 -> julCost += dbc.purchasesList[i].purchaseCost
-                    8 -> augCost += dbc.purchasesList[i].purchaseCost
-                    9 -> sepCost += dbc.purchasesList[i].purchaseCost
-                    10 -> octCost += dbc.purchasesList[i].purchaseCost
-                    11 -> novCost += dbc.purchasesList[i].purchaseCost
-                    12 -> decCost += dbc.purchasesList[i].purchaseCost
+        if (yearGraph != null) { // yearGraph returnerar null efter man lägger till input så det här behövs, inte säker varför den blir null
+            for (i in dbc.purchasesList.indices) {
+                if (dbc.getYear(dbc.purchasesList[i].purchaseDateString) == dbc.getCurrentYear()) {
+                    when (getMonth(dbc.purchasesList[i].purchaseDateString)) {
+                        1 -> janCost += dbc.purchasesList[i].purchaseCost
+                        2 -> febCost += dbc.purchasesList[i].purchaseCost
+                        3 -> marCost += dbc.purchasesList[i].purchaseCost
+                        4 -> aprCost += dbc.purchasesList[i].purchaseCost
+                        5 -> mayCost += dbc.purchasesList[i].purchaseCost
+                        6 -> junCost += dbc.purchasesList[i].purchaseCost
+                        7 -> julCost += dbc.purchasesList[i].purchaseCost
+                        8 -> augCost += dbc.purchasesList[i].purchaseCost
+                        9 -> sepCost += dbc.purchasesList[i].purchaseCost
+                        10 -> octCost += dbc.purchasesList[i].purchaseCost
+                        11 -> novCost += dbc.purchasesList[i].purchaseCost
+                        12 -> decCost += dbc.purchasesList[i].purchaseCost
+                    }
                 }
             }
-        }
 
-        val yearGraph: AAChartModel = AAChartModel()
-            .chartType(AAChartType.Column)
-            .title("Expenses this year")
-            .dataLabelsEnabled(true)
-            .zoomType(AAChartZoomType.XY)
-            .series(
-                arrayOf(
-                    AASeriesElement()
-                        .data(arrayOf(
-                            arrayOf("January", janCost),
-                            arrayOf("February", febCost),
-                            arrayOf("March", marCost),
-                            arrayOf("April", aprCost),
-                            arrayOf("May", mayCost),
-                            arrayOf("June", junCost),
-                            arrayOf("July", julCost),
-                            arrayOf("August", augCost),
-                            arrayOf("September", sepCost),
-                            arrayOf("October", octCost),
-                            arrayOf("November", novCost),
-                            arrayOf("December", decCost),
-                        ))
+            val yearGraph: AAChartModel = AAChartModel()
+                .chartType(AAChartType.Column)
+                .title("Expenses this year")
+                .dataLabelsEnabled(true)
+                .yAxisTitle("SEK")
+                .zoomType(AAChartZoomType.XY)
+                .series(
+                    arrayOf(
+                        AASeriesElement()
+                            .data(arrayOf(
+                                arrayOf("January", janCost),
+                                arrayOf("February", febCost),
+                                arrayOf("March", marCost),
+                                arrayOf("April", aprCost),
+                                arrayOf("May", mayCost),
+                                arrayOf("June", junCost),
+                                arrayOf("July", julCost),
+                                arrayOf("August", augCost),
+                                arrayOf("September", sepCost),
+                                arrayOf("October", octCost),
+                                arrayOf("November", novCost),
+                                arrayOf("December", decCost),
+                            ))
+                    )
                 )
 
-            )
-
-       // year_graph.aa_drawChartWithChartModel(yearGraph)
+             year_graph.aa_drawChartWithChartModel(yearGraph)
+        }
+        else {
+            Log.d("Graph", "yearGraph is null")
+        }
     }
 
     fun pieChart() {
